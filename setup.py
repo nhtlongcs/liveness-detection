492cb198e59e6b9aeb2d48d2252bcc7492abfc57
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-11-13 16:25:37 +0000

    update pkg

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+setup(name="core", version="0.0", package_dir={"core": "."})
+# setup(name="core", version="0.0", package_dir={"core": "src/core", "core.opt":"src/", "core.registry":"src/"})
