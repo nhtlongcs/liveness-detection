f5cd5a0ba98309e211738b95d58b52107d3dfd16
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-11-13 18:24:55 +0000

    fix typo import

--- core/callbacks/__init__.py
+++ core/callbacks/__init__.py
@@ -1,10 +1,8 @@
-from registry import Registry
+from core.registry import Registry
 CALLBACKS_REGISTRY = Registry("CALLBACKS")
 
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, EarlyStopping
-from .visualizer_callbacks import VisualizerCallback
 
 CALLBACKS_REGISTRY.register(EarlyStopping)
 CALLBACKS_REGISTRY.register(ModelCheckpoint)
 CALLBACKS_REGISTRY.register(LearningRateMonitor)
-CALLBACKS_REGISTRY.register(VisualizerCallback)
