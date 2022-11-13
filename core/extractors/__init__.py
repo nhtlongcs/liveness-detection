0fb261557b66dd02149ae851ab57c0f83fc62df4
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-11-13 16:51:52 +0000

    rm transformers (temporary)

--- core/extractors/__init__.py
+++ core/extractors/__init__.py
@@ -4,6 +4,6 @@ EXTRCT_REGISTRY = Registry("EXTRACTOR")
 
 from .eff_net import *
 from .senet import *
-from .hugging import *
+# from .hugging import *
 from .clip.clip_extractors import *
 from .vit_net import *
